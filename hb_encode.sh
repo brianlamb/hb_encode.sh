9b432dded9d75f1a955c102fc4297fd48ba47138
Brian Lamb <brian.lamb@blambstudio.com>
2021-10-04 03:57:16 +0000

    rewrite to handle metadata process all files in directory (loop)
    TODO:
    Check if file is encoded already(e.g. mp4/m4v/mov as source instead of only .mov)
    Copy metadata more thoroughly from 2nd converted file to finished version with other metadata
    Delete orginal source option
    Integrate with Bashew for error handling and command line input processing

--- hb_encode.sh
+++ hb_encode.sh
@@ -1,45 +1,50 @@
-#!/bin/bash
-# hb_encode.sh
+#!/usr/bin/env bash
+
+for file in *.mov
+do
+echo $file
+
+input_file_source="${file##*/}" # strip path
+
+input_file_noextension="${input_file_source%\.[^.]*}" #strip extension
+
+input_file_extension="${input_file_source##*.}" #extract origninal extension
+
+output_extension="mp4"
+
+input_file_converted="${input_file_noextension}.${output_extension}"
+
+output_metadata_fixed="fixed-${input_file_converted}" 
 
 
+##outputdir=$(dirname "${input}")
+##output="$(basename "$input")"
+##output="${outputdir}/${output%.[^.]*}.mp4"
 
-die() {
-    echo "$program: $1" >&2
-    exit ${2:-1}
-}
+# build up optional handbrake options beyond our preset
+handbrake_options="--audio-copy-mask aac --aencoder copy:aac "
+handbrake_options="$handbrake_options --optimize"
+handbrake_options="$handbrake_options --pfr 60"
 
-escape_string() {
-    echo "$1" | sed "s/'/'\\\''/g;/ /s/^\(.*\)$/'\1'/"
-}
+HandBrakeCLI --preset-import-file ../HB1440-60PFR-SlowerRF20.json -Z "My1440-60PFR-Slower-RF20-AACpassthrough-optimizedatom" `${handbrake_options}` -i "${input_file_source}" -o "${input_file_converted}"
+###for i in *.avi; do ffmpeg -i "$i" "${i%.*}.mp4"; done
 
-readonly program="$(basename "$0")"
-readonly input="$1"
 
-if [ ! "$input" ]; then
-    die 'too few arguments'
-fi
+# ...copy metadata over...
+# copy stream
+ffmpeg \
+-i "$input_file_source" \
+-i "$input_file_converted" \
+-map_metadata:g 0:g \
+-map_metadata:s:0 0:s:0 \
+-map_metadata:s:1 0:s:1 \
+-metadata converted="True" \
+-map 1 \
+-codec copy \
+-movflags use_metadata_tags \
+"${output_metadata_fixed}"
 
-handbrake="HandBrakeCLI"
 
-#extract framerate from file input name (if available)
-frame_rate="$(echo "$input" | sed -n 's/.*\([0-9][0-9]\)fps.*/\1/p')"
+touch -r "$input_file_source" "$output_metadata_fixed" 
+done
 
-readonly preset_option="$2"
-handbrake_options=" --preset-import-file $preset_option.json -Z \"$preset_option\""
-input_file="$(basename "$input")"
-output="$(dirname "$input")/${input_file/%\.[^.]*/.mp4}" #.mp4
-
-echo "frame rate: $frame_rate"
-echo "preset $preset_option"
-echo "handbrake options: $handbrake_options"
-
-echo "Encoding: $input" >&2
-
-#time "$handbrake" \
-time \
-echo "$handbrake" \
-    "$handbrake_options" \
-    --input "\"$input\"" \
-    --output "\"$output\"" \
-    --r "$frame_rate" --cfr \
-   2>&1 | tee -a "${output}.log"
